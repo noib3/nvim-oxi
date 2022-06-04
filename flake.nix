e7d2c67234a6b9cd91da4ad3e605bed9c52cf5c2
noib3 <riccardo.mazzarini@pm.me>
2022-06-04 12:21:56 +0200

    :package: add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "nvim-oxi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, ... }@inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlay
+            neovim-nightly-overlay.overlay
+          ];
+        };
+
+        inherit (pkgs) lib stdenv;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = lib.lists.optionals stdenv.isDarwin [ pkgs.libiconv ];
+
+          packages = with pkgs; [
+            gcc
+            rust-bin.nightly.latest.default
+            neovim-nightly
+          ];
+        };
+      });
+}
