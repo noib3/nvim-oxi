bcadbdb66d84c6ccdbf20877a9b93809b7518c21
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-06-08 15:53:21 +0300

    added(api-autocmd bindings): initial commit

--- nvim-oxi/src/api/autocmd.rs
+++ nvim-oxi/src/api/autocmd.rs
@@ -1,1 +1,40 @@
+use nvim_types::{
+    array::Array, dictionary::Dictionary, error::Error as NvimError,
+    object::Object, string::String as NvimString, Integer,
+};
 
+use super::ffi::autocmd::*;
+use super::opts::CreateCommandOpts;
+use super::types::Mode;
+use crate::{
+    api::Buffer,
+    lua::LUA_INTERNAL_CALL,
+    object::{FromObject, ToObject},
+    Result,
+};
+
+pub fn clear_autocmds(opts: &CreateCommandOpts) -> Result<()> {
+    let mut error = NvimError::new();
+
+    unsafe { nvim_clear_autocmds(&(opts.into()), &mut error) };
+    error.into_err_or_else(|| ())
+}
+
+pub fn create_augroup(
+    channel_id: u64,
+    name: &str,
+    opts: &CreateCommandOpts,
+) -> Result<Integer> {
+    let mut error = NvimError::new();
+
+    let result = unsafe {
+        nvim_create_augroup(
+            channel_id,
+            name.into(),
+            &(opts.into()),
+            &mut error,
+        )
+    };
+
+    error.into_err_or_else(|| result)
+}
---
+++ nvim-oxi/src/api/ffi/autocmd.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+use libc::uint64_t;
+use nvim_types::{
+    array::Array, dictionary::Dictionary, error::Error, object::Object,
+    string::String, BufHandle, Integer, TabHandle, WinHandle,
+};
+
+extern "C" {
+    // https://github.com/neovim/neovim/blob/master/src/nvim/api/autocmd.c#L597
+    pub(crate) fn nvim_clear_autocmds(
+        opts: *const Dictionary,
+        err: *mut Error,
+    );
+
+    pub(crate) fn nvim_create_augroup(
+        channel_id: u64,
+        name: String,
+        opts: *const Dictionary,
+        err: *mut Error,
+    ) -> Integer;
+}
