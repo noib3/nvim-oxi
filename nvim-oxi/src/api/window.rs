a05e93a0fbb374a3e5457f628401870b15ac4ec7
noib3 <riccardo.mazzarini@pm.me>
2022-07-02 02:41:17 +0200

    :sparkles: added all bindings to `api-window`

--- nvim-oxi/src/api/window.rs
+++ nvim-oxi/src/api/window.rs
@@ -4,6 +4,7 @@ use nvim_types::{self as nvim, Array, Integer, Object, WinHandle};
 use serde::{Deserialize, Serialize};
 
 use super::ffi::window::*;
+use super::{Buffer, TabPage};
 use crate::lua::LuaFun;
 use crate::object::{FromObject, ToObject};
 use crate::Result;
@@ -65,6 +66,16 @@ impl Window {
         })
     }
 
+    /// Binding to `nvim_win_close`.
+    ///
+    /// Closes the window. When allowed when `textlock` is active.
+    // TODO: take owned `self`?
+    pub fn close(&mut self, force: bool) -> Result<()> {
+        let mut err = nvim::Error::new();
+        unsafe { nvim_win_close(self.0, force, &mut err) };
+        err.into_err_or_else(|| ())
+    }
+
     /// Binding to `nvim_win_del_var`.
     ///
     /// Removes a window-scoped (w:) variable.
@@ -73,6 +84,15 @@ impl Window {
         let name = nvim::String::from(name);
         unsafe { nvim_win_del_var(self.0, name.non_owning(), &mut err) };
         err.into_err_or_else(|| ())
+    }
+
+    /// Binding to `nvim_win_get_buf`.
+    ///
+    /// Gets the current `Buffer` in the window.
+    pub fn get_buf(&self) -> Result<Buffer> {
+        let mut err = nvim::Error::new();
+        let handle = unsafe { nvim_win_get_buf(self.0, &mut err) };
+        err.into_err_or_else(|| handle.into())
     }
 
     /// Binding to `nvim_win_get_cursor`.
@@ -89,6 +109,24 @@ impl Window {
         })
     }
 
+    /// Binding to `nvim_win_get_height`.
+    ///
+    /// Gets the window height as a count of rows.
+    pub fn get_height(&self) -> Result<u32> {
+        let mut err = nvim::Error::new();
+        let height = unsafe { nvim_win_get_height(self.0, &mut err) };
+        err.into_err_or_else(|| height.try_into().expect("always positive"))
+    }
+
+    /// Binding to `nvim_win_get_number`.
+    ///
+    /// Gets the window number.
+    pub fn get_number(&self) -> Result<usize> {
+        let mut err = nvim::Error::new();
+        let nr = unsafe { nvim_win_get_number(self.0, &mut err) };
+        err.into_err_or_else(|| nr.try_into().expect("always positive"))
+    }
+
     /// Binding to `nvim_win_get_position`.
     ///
     /// Gets the window position in display cells.
@@ -101,6 +139,15 @@ impl Window {
             let col = iter.next().unwrap().try_into()?;
             Ok((line, col))
         })
+    }
+
+    /// Binding to `nvim_win_get_tabpage`.
+    ///
+    /// Gets the window's `TabPage`.
+    pub fn get_tabpage(&self) -> Result<TabPage> {
+        let mut err = nvim::Error::new();
+        let handle = unsafe { nvim_win_get_tabpage(self.0, &mut err) };
+        err.into_err_or_else(|| handle.into())
     }
 
     /// Binding to `nvim_win_get_var`.
@@ -117,6 +164,41 @@ impl Window {
         err.into_err_or_flatten(|| V::from_obj(obj))
     }
 
+    /// Binding to `nvim_win_get_width`.
+    ///
+    /// Gets the window width as a number of columns.
+    pub fn get_width(&self) -> Result<u32> {
+        let mut err = nvim::Error::new();
+        let width = unsafe { nvim_win_get_width(self.0, &mut err) };
+        err.into_err_or_else(|| width.try_into().expect("always positive"))
+    }
+
+    /// Binding to `nvim_win_hide`.
+    ///
+    /// Closes the window and hides the buffer it contains.
+    // TODO: take owned `self`?
+    pub fn hide(&mut self) -> Result<()> {
+        let mut err = nvim::Error::new();
+        unsafe { nvim_win_hide(self.0, &mut err) };
+        err.into_err_or_else(|| ())
+    }
+
+    /// Binding to `nvim_win_is_valid`.
+    ///
+    /// Checks if the window is valid.
+    pub fn is_valid(&self) -> bool {
+        unsafe { nvim_win_is_valid(self.0) }
+    }
+
+    /// Binding to `nvim_win_set_buf`.
+    ///
+    /// Sets `buffer` as the current buffer in the window.
+    pub fn set_buf(&mut self, buffer: &Buffer) -> Result<()> {
+        let mut err = nvim::Error::new();
+        unsafe { nvim_win_set_buf(self.0, buffer.0, &mut err) };
+        err.into_err_or_else(|| ())
+    }
+
     /// Binding to `nvim_win_set_cursor`.
     ///
     /// Sets the (1,0)-indexed cursor in the window. This will scroll the
@@ -125,6 +207,15 @@ impl Window {
         let mut err = nvim::Error::new();
         let pos = Array::from_iter([line as Integer, col as Integer]);
         unsafe { nvim_win_set_cursor(self.0, pos.non_owning(), &mut err) };
+        err.into_err_or_else(|| ())
+    }
+
+    /// Binding to `nvim_win_set_height`.
+    ///
+    /// Sets the window height.
+    pub fn set_height(&mut self, height: impl Into<u32>) -> Result<()> {
+        let mut err = nvim::Error::new();
+        unsafe { nvim_win_set_height(self.0, height.into().into(), &mut err) };
         err.into_err_or_else(|| ())
     }
 
@@ -142,6 +233,15 @@ impl Window {
                 &mut err,
             )
         };
+        err.into_err_or_else(|| ())
+    }
+
+    /// Binding to `nvim_win_set_width`.
+    ///
+    /// Sets the window width.
+    pub fn set_width(&mut self, width: impl Into<u32>) -> Result<()> {
+        let mut err = nvim::Error::new();
+        unsafe { nvim_win_set_width(self.0, width.into().into(), &mut err) };
         err.into_err_or_else(|| ())
     }
 }
