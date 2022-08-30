708eaf87dfeb893dfd4d55760c647343f9fae102
zjp <jiping_zhou@foxmail.com>
2022-08-30 22:46:22 +0800

    update(example): testing api

--- examples/api/run.sh
+++ examples/api/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 export lua="
-require'api'
+local api = require'api'
+api.open_window()
+api.close_window()
+local k = vim.api.nvim_replace_termcodes('iFrom insert mode: hi<esc>', true, false, true)
+vim.api.nvim_feedkeys(k, 'x', true)
+print(vim.api.nvim_buf_get_lines(0, 0, -1, false)[1])
+vim.cmd [[ Greetings ]]
+vim.cmd [[q!]]
 "
 bash ../run.sh
