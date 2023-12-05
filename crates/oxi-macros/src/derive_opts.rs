cfbbd5281133a600ffe06b808a2528dedb3f767c
Riccardo Mazzarini <me@noib3.dev>
2023-12-05 16:06:23 +0100

    macros: implement setters generated by `OptsBuilder`

--- crates/oxi-macros/src/derive_opts.rs
+++ crates/oxi-macros/src/derive_opts.rs
@@ -1,3 +1,5 @@
+use core::cmp::Ordering;
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::*;
@@ -6,6 +8,7 @@ struct OptsField {
     name: Ident,
     ty: Type,
     doc_comment: Option<String>,
+    mask_idx: usize,
 }
 
 impl From<&Field> for OptsField {
@@ -14,6 +17,7 @@ impl From<&Field> for OptsField {
             name: field.ident.as_ref().unwrap().clone(),
             ty: field.ty.clone(),
             doc_comment: parse_doc_comment(field),
+            mask_idx: 0,
         }
     }
 }
@@ -44,21 +48,44 @@ pub fn derive_opts_builder(attr: TokenStream) -> TokenStream {
     let mut opts_fields =
         fields.named.iter().skip(1).map(OptsField::from).collect::<Vec<_>>();
 
+    let mut mask_ordering = (0..opts_fields.len()).collect::<Vec<_>>();
+
+    mask_ordering.sort_by(|&left_idx, &right_idx| {
+        let left_field = &opts_fields[left_idx];
+        let right_field = &opts_fields[right_idx];
+
+        let left_name = left_field.name.to_string();
+        let right_name = right_field.name.to_string();
+
+        match left_name.len().cmp(&right_name.len()) {
+            Ordering::Equal => left_name.cmp(&right_name),
+            ordering => ordering,
+        }
+    });
+
+    for (mask_idx, opts_idx) in mask_ordering.into_iter().enumerate() {
+        opts_fields[opts_idx].mask_idx = mask_idx;
+    }
+
     let builder_name =
         Ident::new(&format!("{}Builder", struct_name), struct_name.span());
 
     let builder_method_doc_comment =
         format!("Creates a new [`{builder_name}`].");
 
+    let mask_name = first_field.ident.as_ref().unwrap();
+
     let setters = opts_fields.iter().map(|field| {
         let field_doc_comment = &field.doc_comment;
+        let field_mask_idx = &field.mask_idx;
         let field_name = &field.name;
         let field_ty = &field.ty;
         quote! {
             #[doc = #field_doc_comment]
             #[inline]
             pub fn #field_name(&mut self, #field_name: #field_ty) -> &mut Self {
-                /// TODO: update the field and the mask
+                self.0.#field_name = #field_name;
+                self.0.#mask_name |= (1 << (#field_mask_idx + 1)) + 1;
                 self
             }
         }
