6c1f2130c9c7f88982764d9dcad0d0b2165471af
Riccardo Mazzarini <me@noib3.dev>
2024-02-04 19:32:57 +0100

    macros: move `module` to its own module

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,14 +1,17 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::parse_macro_input;
 
 mod derive_opts;
+
+#[cfg(feature = "module")]
+mod module;
 
 /// TODO: docs
 #[proc_macro_derive(OptsBuilder, attributes(builder))]
 pub fn derive_opts_builder(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as syn::DeriveInput);
     derive_opts::expand_derive_opts_builder(&input)
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
@@ -32,25 +35,7 @@ pub fn derive_opts_builder(input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn module(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let item = parse_macro_input!(item as syn::ItemFn);
-
-    #[allow(clippy::redundant_clone)]
-    let module_name = item.sig.ident.clone();
-
-    let lua_module =
-        Ident::new(&format!("luaopen_{module_name}"), Span::call_site());
-
-    let module_body = quote! {
-        #item
-
-        #[no_mangle]
-        unsafe extern "C" fn #lua_module(
-            state: *mut ::nvim_oxi::lua::ffi::lua_State,
-        ) -> ::std::ffi::c_int {
-            ::nvim_oxi::entrypoint(state, #module_name)
-        }
-    };
-
-    module_body.into()
+    module::module(item).into()
 }
 
 /// Tests a piece of code inside a Neovim session.
