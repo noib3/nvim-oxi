231e524a8e8c110b22cadc974a2957a4b6c10bab
Riccardo Mazzarini <me@noib3.dev>
2024-04-23 13:22:31 +0200

    build: allow setting more than one `neovim-*` feature

--- build.rs
+++ build.rs
@@ -8,21 +8,6 @@ compile_error!(
      neovim-nightly"
 );
 
-#[cfg(all(
-    feature = "neovim-0-8",
-    any(feature = "neovim-0-9", feature = "neovim-nightly")
-))]
-compile_error!(
-    "You can only enable one of the features: neovim-0-8, neovim-0-9, \
-     neovim-nightly"
-);
-
-#[cfg(all(feature = "neovim-0-9", feature = "neovim-nightly"))]
-compile_error!(
-    "You can only enable one of the features: neovim-0-8, neovim-0-9, \
-     neovim-nightly"
-);
-
 fn main() {
     println!("cargo:rerun-if-changed=build");
 }
