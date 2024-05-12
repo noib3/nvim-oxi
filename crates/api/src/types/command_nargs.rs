cbc9c1da3f8ebd289809b6b4d31698bee9d3da16
MyyPo <nikirsmcgl@gmail.com>
2024-05-12 22:54:21 +0300

    api: fix `CommandNArgs` `Zero` and `One` int representation

--- crates/api/src/types/command_nargs.rs
+++ crates/api/src/types/command_nargs.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{ser, Deserialize, Serialize};
 use types::{
     conversion::{self, ToObject},
     serde::Serializer,
@@ -7,9 +7,7 @@ use types::{
 
 /// Number of arguments accepted by a command.
 #[non_exhaustive]
-#[derive(
-    Copy, Clone, Debug, Default, Eq, PartialEq, Hash, Serialize, Deserialize,
-)]
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash, Deserialize)]
 pub enum CommandNArgs {
     #[default]
     #[serde(rename = "0")]
@@ -26,6 +24,22 @@ pub enum CommandNArgs {
 
     #[serde(rename = "*")]
     Any,
+}
+
+// https://github.com/serde-rs/serde/issues/1773
+impl Serialize for CommandNArgs {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        match self {
+            CommandNArgs::Zero => serializer.serialize_i32(0),
+            CommandNArgs::One => serializer.serialize_i32(1),
+            CommandNArgs::ZeroOrOne => serializer.serialize_str("?"),
+            CommandNArgs::OneOrMore => serializer.serialize_str("+"),
+            CommandNArgs::Any => serializer.serialize_str("*"),
+        }
+    }
 }
 
 impl ToObject for CommandNArgs {
