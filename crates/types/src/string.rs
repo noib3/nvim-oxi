6f5f735099eb4b398584813ff43050e55c5dc68c
MarcusGrass <34198073+MarcusGrass@users.noreply.github.com>
2025-03-04 12:46:24 +0100

    fix: Use platform specific c_char in malloc cast (#221)

--- crates/types/src/string.rs
+++ crates/types/src/string.rs
@@ -251,7 +251,7 @@ impl StringBuilder {
             // terminated by a null byte.
             if s.data.is_null() {
                 unsafe {
-                    let ptr = libc::malloc(1) as *mut i8;
+                    let ptr = libc::malloc(1) as *mut ffi::c_char;
                     if ptr.is_null() {
                         unable_to_alloc_memory();
                     }
