de86c8a96472f951019587d3801cd2b3f9cfdce7
Riccardo Mazzarini <me@noib3.dev>
2025-10-14 18:23:01 +0200

    fix(api): add `EchoOpts::err` on 0.11

--- crates/api/src/opts/echo.rs
+++ crates/api/src/opts/echo.rs
@@ -2,6 +2,8 @@
 #[derive(Clone, Debug, Default)]
 #[repr(C)]
 pub struct EchoOpts {
+    #[cfg(feature = "neovim-0-11")] // Only on 0.11.
+    err: bool,
     verbose: bool,
 }
 
@@ -16,6 +18,14 @@ impl EchoOpts {
 pub struct EchoOptsBuilder(EchoOpts);
 
 impl EchoOptsBuilder {
+    #[cfg_attr(docsrs, doc(cfg(feature = "neovim-0-11")))]
+    #[cfg(feature = "neovim-0-11")] // Only on 0.11.
+    #[inline]
+    pub fn err(&mut self, err: bool) -> &mut Self {
+        self.0.err = err;
+        self
+    }
+
     #[inline]
     pub fn verbose(&mut self, verbose: bool) -> &mut Self {
         self.0.verbose = verbose;
